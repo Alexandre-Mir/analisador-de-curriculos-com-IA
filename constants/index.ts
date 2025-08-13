231840e444363214b7ac474cfcfb8a059ff82365
Alexandre-Mir <xandenextel@gmail.com>
2025-08-13 18:03:08 -0300

    Cloud storage, PDF-Image conversion, Key-value storage and AI feedback

--- constants/index.ts
+++ constants/index.ts
@@ -137,8 +137,7 @@ export const AIResponseFormat = `
 
 export const prepareInstructions = ({
   jobTitle,
-  jobDescription,
-  AIResponseFormat,
+  jobDescription, 
 }: {
   jobTitle: string;
   jobDescription: string;
